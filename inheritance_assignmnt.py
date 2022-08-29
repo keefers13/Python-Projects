9e45b83556fac636b0fc3f9aaeaf5595adb7bf4d
keefers13 <keithmoore13@gmail.com>
2022-08-29 11:04:06 -0600

    pushing to main
    pushing to main

---
+++ inheritance_assignmnt.py
@@ -0,0 +1,15 @@
+
+class faculty:
+    name = "No Name Provided"
+    email = ''
+    password = '1234abcd'
+    subject = ''
+    account_number = 0
+
+class tenured(faculty):
+    PhD_type = ''
+    years_tenured = 0
+
+class adjunct(faculty):
+    highest_degree_earned = ''
+    years_employed = 0

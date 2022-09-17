b07d47ce81b66af3c1e2eec843610a46ae320262
keefers13 <keithmoore13@gmail.com>
2022-09-16 21:04:52 -0600

    pushing to main
    push to main

---
+++ DjangoUniversity/Classes/campusApp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Creates model of the University Campus
+class UniversityCampus(models.Model):
+    campus_name = models.CharField(max_length=60, default="", blank=True, null=False)
+    state = models.CharField(max_length=2, default="", blank=True, null=False)
+    campus_id = models.IntegerField(default="", blank=True, null=False)
+
+
+    # Creates model manager
+    objects = models.Manager()
+
+    # Displays the object output values in the form of a string
+    def __str__(self):
+        # Returns the input of the title and the instructor name
+        # field as a tuple to display in the browser instead of the default titles
+        display_campus = '{0.campus_name}: {0.state}'
+        return display_campus.format(self)
+
+    # Removes added 's' that Django adds to the model name in the browser display
+    class Meta:
+        verbose_name_plural = "University Campus"
+
+
+
